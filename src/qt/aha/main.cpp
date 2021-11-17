32fafd7d3524b419434ee51f7b14682d88689250
cameronbrownn <cameronbrown1301@gmail.com>
2021-11-17 14:22:51 -0600

    Update main.cpp

--- src/qt/aha/main.cpp
+++ src/qt/aha/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwin.h"
+#include "databasehandler.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    DatabaseHandler dbhandler;
     MainWin w;
     w.show();
     return a.exec();
