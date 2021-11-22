7fe8ae76a532c570f18ba2830ef7af7dc4551345
Angrycat1995 <huy.nguyen3@mavs.uta.edu>
2021-11-22 13:14:50 -0600

    Update main.cpp

--- src/qt/aha/main.cpp
+++ src/qt/aha/main.cpp
@@ -1,11 +1,22 @@
 #include "mainwin.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
     MainWin w;
+
+    //set the app syle sheet
+    QFile styleSheetFile(":/style/style/Toolery/Toolery.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet{QLatin1String (styleSheetFile.readAll())};
+    app.setStyleSheet(styleSheet);
+
+
+    w.setWindowState(Qt::WindowMaximized);
     w.show();
-    return a.exec();
+    return app.exec();
 }
