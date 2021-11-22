e4b1d487f289d198c9bc1649a2162cf9d5e97e1b
Angrycat1995 <huy.nguyen3@mavs.uta.edu>
2021-11-22 13:14:53 -0600

    Update mainwin.cpp

--- src/qt/aha/mainwin.cpp
+++ src/qt/aha/mainwin.cpp
@@ -1,6 +1,8 @@
 #include "mainwin.h"
 #include "ui_mainwin.h"
 #include "loginwin.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWin::MainWin(QWidget *parent)
     : QMainWindow(parent)
@@ -14,6 +16,25 @@ MainWin::MainWin(QWidget *parent)
     if(m_currentUser == 1)
     {
         ui->setupUi(this);
+        ui->customplot->addGraph();
+        ui->customplot->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+        ui->customplot->graph(0)->setLineStyle(QCPGraph::lsLine);
+        ui->customplot->xAxis->setLabel("X");
+        ui->customplot->yAxis->setLabel("Y");
+        ui->customplot->xAxis->setRange(0,1000);
+        ui->customplot->yAxis->setRange(0,1000);
+        ui->customplot->setInteractions(QCP::iRangeDrag|QCP::iRangeZoom|QCP::iSelectPlottables);
+
+        QVector <double> x={1,2,3,4,5},y={0,9,18,5,30};
+        ui->customplot->graph(0)->setData(x,y);
+        ui->customplot->rescaleAxes();
+        ui->customplot->replot();
+        ui->customplot->update();
+
+//        ui->comboBox->addItem("Career and education");
+//        ui->comboBox->addItem("News and media");
+//        ui->comboBox->addItem("Internet and telecom");
+
     }
     else
     {
