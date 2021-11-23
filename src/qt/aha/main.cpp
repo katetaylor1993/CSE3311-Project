2fc9ee7523dbadff48c8e54a1cf9cb8e551692a7
cameronbrownn <cameronbrown1301@gmail.com>
2021-11-22 19:27:42 -0600

    Update main.cpp

--- src/qt/aha/main.cpp
+++ src/qt/aha/main.cpp
@@ -6,7 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    DatabaseHandler dbhandler;
+    DatabaseHandler dbHandler;
+    dbHandler.setAPIKey("AIzaSyD_xlNz0ZkNP0GMwpBEgpvVudJb4v0v6Z8");
+    //dbHandler.signUserUp( "NewUser4Email@gmail.com", "Password123!");
+    dbHandler.signUserIn( "C55@gmail.com", "Password123!");
     MainWin w;
     w.show();
     return a.exec();
