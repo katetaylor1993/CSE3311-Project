b00885a4613749bf2cb7c16505baea2c44fa76c4
cameronbrownn <cameronbrown1301@gmail.com>
2021-11-22 19:28:02 -0600

    Update databasehandler.h

--- src/qt/aha/databasehandler.h
+++ src/qt/aha/databasehandler.h
@@ -1,9 +1,10 @@
-#ifndef DATABASEHANDLER_H
+ #ifndef DATABASEHANDLER_H
 #define DATABASEHANDLER_H
 
 #include <QObject>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+#include <QJsonDocument>
 
 class DatabaseHandler : public QObject
 {
@@ -12,14 +13,28 @@ public:
     explicit DatabaseHandler(QObject *parent = nullptr);
     ~DatabaseHandler();
 
+    // Database Authorization
+    void setAPIKey( const QString & apiKey);
+    void signUserUp( const QString & emailAddress, const QString & password);
+    void signUserIn( const QString & emailAddress, const QString & password);
+
 public slots:
     void networkReplyReadyRead();
+    void performAuthenticatedDatabaseCall();
 
 signals:
-
+    void userSignedIn();
 private:
     QNetworkAccessManager * m_networkManager;
     QNetworkReply * m_networkReply;
+
+    // Database Authorization
+    QString m_apiKey;
+    QString m_idToken;
+    void performPOST( const QString & url, const QJsonDocument & payload );
+    void parseResponse( const QByteArray & response);
+
+
 };
 
 #endif // DATABASEHANDLER_H
