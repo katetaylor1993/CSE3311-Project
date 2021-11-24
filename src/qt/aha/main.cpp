f32fbb76714cf3501f3c6188739d5d495493a620
Angrycat1995 <huy.nguyen3@mavs.uta.edu>
2021-11-24 11:56:17 -0600

    Update main.cpp

--- src/qt/aha/main.cpp
+++ src/qt/aha/main.cpp
@@ -14,13 +14,6 @@ int main(int argc, char *argv[])
 
     MainWin w;
 
-    //set the app syle sheet
-    QFile styleSheetFile(":/style/style/Toolery/Toolery.qss");
-    styleSheetFile.open(QFile::ReadOnly);
-    QString styleSheet{QLatin1String (styleSheetFile.readAll())};
-    app.setStyleSheet(styleSheet);
-
-
     w.setWindowState(Qt::WindowMaximized);
     w.setWindowTitle("Aha");
     w.show();
