3dcc87ece4f4e93756c2fa8ab398f538d2dcf6c8
Angrycat1995 <huy.nguyen3@mavs.uta.edu>
2021-11-27 14:27:21 -0600

    Update mainwin.cpp

--- src/qt/aha/mainwin.cpp
+++ src/qt/aha/mainwin.cpp
@@ -19,6 +19,63 @@ MainWin::MainWin(QWidget *parent)
     if(m_currentUser == 1)
     {
         ui->setupUi(this);
+
+
+        QBarSet *set0 = new QBarSet("Bob");
+        QBarSet *set1 = new QBarSet("Tom");
+        QBarSet *set2 = new QBarSet("John");
+        QBarSet *set3 = new QBarSet("Doe");
+        QBarSet *set4 = new QBarSet("Ahmad");
+
+        *set0 << 1 << 2 << 3 << 4 << 5 << 6;
+        *set1 << 5 << 0 << 0 << 4 << 0 << 7;
+        *set2 << 3 << 5 << 8 << 13 << 8 << 5;
+        *set3 << 5 << 6 << 7 << 3 << 4 << 5;
+        *set4 << 9 << 7 << 5 << 3 << 1 << 2;
+
+        QBarSeries *series= new QBarSeries();
+        series->append(set0);
+        series->append(set1);
+        series->append(set2);
+        series->append(set3);
+        series->append(set4);
+
+        QChart *chart=new QChart();
+        chart->addSeries(series);
+        chart->setTitle("Bar Chart");
+        chart->setAnimationOptions(QChart::SeriesAnimations);
+
+        QStringList categories;
+        categories << "Jan" << "Feb" << "Mar" << "Apr"<<"May"<< "Jun";
+        categories << "Jan" << "Feb" << "Mar" << "Apr" << "May" << "Jun";
+        QBarCategoryAxis *axisX = new QBarCategoryAxis();
+        axisX->append(categories);
+        chart->addAxis(axisX, Qt::AlignBottom);
+        series->attachAxis(axisX);
+
+        QValueAxis *axisY = new QValueAxis();
+        axisY->setRange(0,15);
+        chart->addAxis(axisY, Qt::AlignLeft);
+        series->attachAxis(axisY);
+
+        chart->legend()->setVisible(true);
+        chart->legend()->setAlignment(Qt::AlignBottom);
+
+        QChartView *chartView=new QChartView(chart);
+        chartView->setRenderHint(QPainter::Antialiasing);
+        chartView->setParent(ui->horizontalFrame);
+
+
+
+
+
+
+
+
+
+
+
+
         ui->line_chart->addGraph();
         ui->line_chart->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
         ui->line_chart->graph(0)->setLineStyle(QCPGraph::lsLine);
