a5364ba397748af98334cfa4a9fc9372e339c196
Angrycat1995 <huy.nguyen3@mavs.uta.edu>
2021-11-27 13:12:59 -0600

    Update main.cpp

--- src/qt/aha/main.cpp
+++ src/qt/aha/main.cpp
@@ -18,43 +18,11 @@ int main(int argc, char *argv[])
 
     DatabaseHandler::getInstance()->connectToDB();
 
-
     MainWin w;
-
 
     w.setWindowState(Qt::WindowMaximized);
     w.setWindowTitle("Aha");
     w.show();
-
-//    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-//    db.setHostName("remotemysql.com");
-//    db.setDatabaseName("JfadHzJmgq");
-//    db.setUserName("JfadHzJmgq");
-//    db.setPassword("mpPExijexc");
-
-
-//    if(db.open()){
-//        qDebug("Databse Connected!");
-//        //QMessageBox::information(this,"Connection","Databse Connected.");
-//    }else{
-//        qDebug("Cannnot connected to database.");
-//        //QMessageBox::information(this,"Connection","Cannnot connected to database.");
-//    }
-
-//    QString emp_username;
-//    QSqlQuery query;
-//    query.prepare("SELECT emp_username FROM supervises WHERE super_username='super1';");
-//    query.exec();
-//    if(query.size()>0){
-//        while(query.next()){
-//            emp_username=query.value("emp_username").toString().toUtf8().constData();
-//            qDebug() << emp_username;
-
-//        }
-
-//    }else{
-
-//    }
 
     return app.exec();
 }
